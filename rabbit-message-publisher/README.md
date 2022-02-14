05769e8a63c0a1527137824ac39ebfd8002c4e09
Balint Bende <balint@bimspot.io>
2022-02-14 13:33:43 +0100

    [BMSPET-804] fixed publisher, updated readme

--- rabbit-message-publisher/README.md
+++ rabbit-message-publisher/README.md
@@ -9,14 +9,13 @@ The script is used for publishing RabbitMQ message from the provided arguments.
 Usage: publisher.js [options]
 
 Options:
-      --version  Show version number                                   [boolean]
-  -u, --uri      RabbitMQ amqp uri.                                   [required]
-  -q, --queue    Name of the queue to send to.                        [required]
-  -m, --message  Message to be sent.                                  [required]
-  -d, --durable  Optionally the queue will survive the broker restart. [boolean]
-  -h, --help     Show help                                             [boolean]
+      --version   Show version number                                  [boolean]
+  -u, --uri       RabbitMQ amqp uri.                                  [required]
+  -e, --exchange  Topic exchange to which the message is sent.        [required]
+  -k, --key       Routing key.                                        [required]
+  -m, --message   Message to be sent.                                 [required]
+  -d, --durable   Optionally the queue will survive the broker restart.[boolean]
+  -h, --help      Show help                                            [boolean]
 
 Examples:
-  publisher.js -u amqp://localhost -q hell o -d
-
-  The publisher will connect to RabbitMQ, send a single message, then exit.
+  publisher.js -u amqp://localhost -e file -k file.event.created -m {} -d
--- rabbit-message-publisher/publisher.js
+++ rabbit-message-publisher/publisher.js
@@ -3,23 +3,18 @@ import yargs from 'yargs'
 
 var argv = yargs(process.argv.slice(2))
     .usage('Usage: $0 [options]')
-    .example('$0 -u amqp://localhost -q hello -d', 
+    .example('$0 -u amqp://localhost -e file -k file.event.created -m {} -d', 
     'The publisher will connect to RabbitMQ, send a single message, then exit.')
-    .alias('u', 'uri')
-    .nargs('u', 1)
-    .describe('u', 'RabbitMQ amqp uri.')
-    .alias('q', 'queue')
-    .nargs('q', 1)
-    .describe('q', 'Name of the queue to send to.')
-    .alias('m', 'message')
-    .nargs('m', 1)
-    .describe('m', 'Message to be sent.')
+    .alias('u', 'uri').nargs('u', 1).describe('u', 'RabbitMQ amqp uri.')
+    .alias('e', 'exchange').nargs('e', 1).describe('e', 'Topic exchange to which the message is sent.')
+    .alias('k', 'key').nargs('k', 1).describe('k', 'Routing key.')
+    .alias('m', 'message').nargs('m', 1).describe('m', 'Message to be sent.')
     .option('durable', {
       alias: 'd',
       type: 'boolean',
       description: 'Optionally the queue will survive the broker restart.'
     })
-    .demandOption(['u', 'q', 'm'])
+    .demandOption(['u', 'e', 'k', 'm'])
     .help('h')
     .alias('h', 'help')
     .argv;
@@ -33,11 +28,13 @@ connect(argv.uri, (error, connection) => {
     if (error) {
       throw error
     }
-    channel.assertQueue(queueName, {
-      durable: argv.durable
+    // durable false by default
+    let durable = argv.durable || false
+    channel.assertExchange(argv.exchange, 'topic', {
+      durable: durable
     })
-    channel.sendToQueue(argv.queue, Buffer.from(argv.message))
-    console.log(" [x] Sent %s", msg)
+    channel.publish(argv.exchange, argv.key, Buffer.from(argv.message))
+    console.log(" [x] Sent %s: $s", argv.key, argv.message)
 
     setTimeout(() => {
       connection.close();
