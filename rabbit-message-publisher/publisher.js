e962e070b474bd8fe5610cd1b6eb915ccecea437
Adam Eri <adam@bimspot.io>
2022-02-15 17:25:08 +0100

    Async stuff

--- rabbit-message-publisher/publisher.js
+++ rabbit-message-publisher/publisher.js
@@ -1,45 +1,54 @@
-// import connect from 'amqplib/callback_api'
+import * as amqp from 'amqplib'
 import yargs from 'yargs'
-
-var argv = yargs(process.argv.slice(2))
+;(async () => {
+  const argv = yargs(process.argv.slice(2))
     .usage('Usage: $0 [options]')
-    .example('$0 -u amqp://localhost -e file -t topic -k file.event.created -m {} -d',
-    'The publisher will connect to RabbitMQ, send a single message, then exit.')
-    .alias('u', 'uri').nargs('u', 1).describe('u', 'RabbitMQ amqp uri.')
-    .alias('e', 'exchange').nargs('e', 1).describe('e', 'Topic exchange to which the message is sent.')
-    .alias('t', 'type').nargs('t', 1).describe('e', 'The type of he exchange (direct, topic, fanout, match).')
-    .alias('k', 'key').nargs('k', 1).describe('k', 'Routing key.')
-    .alias('m', 'message').nargs('m', 1).describe('m', 'Message to be sent.')
+    .example(
+      '$0 -u amqp://localhost -e file -t topic -k file.event.created -m {} -d',
+      'The publisher will connect to RabbitMQ, send a single message, then exit.',
+    )
+    .alias('u', 'uri')
+    .nargs('u', 1)
+    .describe('u', 'RabbitMQ amqp uri.')
+    .alias('e', 'exchange')
+    .nargs('e', 1)
+    .describe('e', 'Topic exchange to which the message is sent.')
+    .alias('t', 'type')
+    .nargs('t', 1)
+    .describe('e', 'The type of he exchange (direct, topic, fanout, match).')
+    .alias('k', 'key')
+    .nargs('k', 1)
+    .describe('k', 'Routing key.')
+    .alias('m', 'message')
+    .nargs('m', 1)
+    .describe('m', 'Message to be sent.')
+    .alias('vn', 'connectionName')
+    .nargs('cn', 1)
+    .describe('cn', 'The name of the connection to be created.')
     .option('durable', {
       alias: 'd',
       type: 'boolean',
-      description: 'Optionally the queue will survive the broker restart.'
+      description: 'Optionally the queue will survive the broker restart.',
     })
     .demandOption(['u', 'e', 't', 'k', 'm'])
     .help('h')
-    .alias('h', 'help')
-    .argv;
+    .alias('h', 'help').argv
 
-connect(argv.uri, (error, connection) => {
-  if (error) {
-    throw error
-  }
+  const connection = await amqp.connect(argv.uri, {
+    clientProperties: { connection_name: argv.connectionName },
+  })
 
-  connection.createChannel((error, channel) => {
-    if (error) {
-      throw error
-    }
-    // durable false by default
-    let durable = argv.durable || false
-    channel.assertExchange(argv.exchange, argv.type, {
-      durable: durable
-    })
-    channel.publish(argv.exchange, argv.key, Buffer.from(argv.message))
-    console.log(" [x] Sent %s: $s", argv.key, argv.message)
+  const channel = await connection.createChannel()
 
-    setTimeout(() => {
-      connection.close();
-      process.exit(0)
-      }, 500);
-  });
-})
+  const durable = argv.durable || false
+  await channel.assertExchange(argv.exchange, argv.type, {
+    durable: durable,
+  })
+
+  channel.publish(argv.exchange, argv.key, Buffer.from(argv.message))
+
+  setTimeout(async () => {
+    await connection.close()
+    process.exit(0)
+  }, 500)
+})()
